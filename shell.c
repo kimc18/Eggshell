79a2d0e8c0401f8ef34ca4353e58b7d0c62c3fc8
Kim  Camilleri <kim.camilleri.18@um.edu.mt>
2020-06-28 09:18:14 +0000

    Update shell.c

--- shell.c
+++ shell.c
@@ -135,7 +135,7 @@ int main(int argc, char** argv)
         }
 
         //changes directory
-        else if (strcmp(line, "cd..") == 0)
+        else if (strcmp(line, "cd ..") == 0)
         {
             chdir("..");
         }
